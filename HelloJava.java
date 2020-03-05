b3ea5334c37c471d770e99ae11fdab9418c01be3
veera <veeraiah.cheekati@kritikalvision.ai>
2020-03-05 17:55:07 +0530

    modified in frst line

--- HelloJava.java
+++ HelloJava.java
@@ -3,7 +3,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 
                 System.out.println("Hello Java1 new change11111");
-		System.out.println("Hello Java dev2 change2");
+		System.out.println("Hello Java dev2 change22222");
                   
 		  
 		  
